1e59ffd142b1f1529ea3ff06548f262865aaabfe
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-14 09:59:08 -0600

    Actualizacion de la ruta, para requerir permisos de administrador o cliente.

--- Tarea1/src/routes/users.ts
+++ Tarea1/src/routes/users.ts
@@ -1,10 +1,10 @@
 import { Router } from "express"
 import usersController, { UsersController } from "../controllers/user.controllers";
-import { authMiddleware } from "../middlewares/auth";
+import { authMiddleware, authMiddlewareRoles } from "../middlewares/auth";
 
 const router = Router();
 
-router.get('', authMiddleware ,usersController.getAll);
+router.get('', authMiddleware, authMiddlewareRoles(['admin', 'gerente']), usersController.getAll);
 
 router.get('/2', usersController.findAll);
 
