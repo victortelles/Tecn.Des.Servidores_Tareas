eefe159cac994e388e75f0b44886ba30103ac9a1
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-14 10:02:36 -0600

    0. Actualizacion de las claves (se añadio otra ) 1. Modificacion de usuarios, para asignar varios (hardcordeados) 2. Actualizacion de la funcion de autenticacion por clave para asignar el rol 3. Implementacion de otra funcion para autentticar el rol y hacer llamar la funcion cuando tenga la clave de algun permiso.

--- Tarea1/src/middlewares/auth.ts
+++ Tarea1/src/middlewares/auth.ts
@@ -1,24 +1,62 @@
 import {Request, Response, NextFunction } from "express";
 import { User } from "../types/user";
 
-const secretKey = '12345';
+//llaves
+const secretKeyAdmin = '12345';
+const secretKeyGerente = '67890';
 
-declare namespace Express {
-    export interface Request {
-        user?: User
+//Usuarios Hardcodeados
+export const users: User[] = [
+    {
+        id: '1',
+        name: 'Pedro',
+        role: 'admin'
+    },
+    {
+        id: '2',
+        name: 'Juan',
+        role: 'gerente'
+    },
+    {
+        id: '3',
+        name: 'Maria',
+        role: 'cliente'
+    },
+]
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: User
+        }
     }
 }
 
+//Validar clave
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
     //const token = req.headers.authorization;
     const key = req.query.key;
-    if (key === secretKey) {
-        const user: User = {
-            id: '1',
-            name: 'John',
-        }
+
+    //asignacion de rol por el usuario
+    const user = users.find(u => {
+        return (key === secretKeyAdmin && u.role === 'admin') ||
+                (key === secretKeyGerente && u.role === 'gerente') ||
+                (key !== secretKeyAdmin && key !== secretKeyGerente && u.role === 'cliente');
+    });
+
+    if(user) {
+        req.user = user;
         return next();
     }
-
     res.sendStatus(401);
+}
+
+//Validacion de roles
+export function authMiddlewareRoles(roles: string[]) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (req.user && roles.includes(req.user.role)) {
+            return next()
+        }
+        res.sendStatus(403);
+    };
 }
