2eb429316e6fdc334aec5c4893f8de082f09b00b
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-24 23:02:04 -0600

    Creacion de archivos & carpetas, para implementar la encriptacion y el JWT

---
+++ Tarea 2/src/controllers/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Request, response, Response } from "express";
+import { HTTP_STATUS_CODE } from "../types/http-status-codes";
+import User from "../models/user";
+
+class UsersController {
+
+}
+
+const controller = new UsersController();
+//Exportacion
+export default controller;
