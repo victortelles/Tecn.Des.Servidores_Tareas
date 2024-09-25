9fa20b9c314af0d0f87cce38a7efbfd27d0aa40c
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-24 19:51:08 -0600

    Interfaz del usuario

---
+++ Tarea 2/src/types/user.ts
@@ -0,0 +1,9 @@
+//Interfaz del usuario
+export interface IUser {
+    firstName: String;
+    lastName: String;
+    email: String;
+    password: String;
+    role: String;
+    status?: String;
+}
