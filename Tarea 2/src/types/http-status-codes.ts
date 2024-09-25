739b40ac5778e24d4e83e91b9e7914d06f00da31
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-24 19:50:56 -0600

    Codigos HTTP STATUS

---
+++ Tarea 2/src/types/http-status-codes.ts
@@ -0,0 +1,9 @@
+//Enum de lista de status code
+export enum HTTP_STATUS_CODE {
+    SUCCESS = 200,
+    BAD_REQUEST = 400,
+    AUTORIZATION= 401,
+    FORBIDEN = 403,
+    NOT_FOUND = 404,
+    SERVER_ERROR = 500,
+}
