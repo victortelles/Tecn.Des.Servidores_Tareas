adfd6a6a342f939913b3df62a1b2136fe3403575
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-07 22:46:54 -0600

    Renombramiento de la carpeta

--- Tarea 3/src/routes/upload.ts
+++ Tarea 3/src/routes/upload.ts
@@ -5,7 +5,7 @@ import { HTTP_STATUS_CODES } from "../types/http-status-codes";
 
 const router = Router();
 
-router.post('/', (req, res) => {
+router.post('', (req, res) => {
     //validacion si son varios archivos
     const isMultiple = Array.isArray(req.body.docs);
     const uploadHandler = isMultiple
