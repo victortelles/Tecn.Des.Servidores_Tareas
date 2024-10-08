268ae43efdeffffd3ceea4c0547f80438c0744b4
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-07 21:51:11 -0600

    Implementacion upload

--- tarea3/src/middlewares/upload.ts
+++ tarea3/src/middlewares/upload.ts
@@ -1,24 +1,29 @@
 import { Request } from "express";
 import multer, { diskStorage, FileFilterCallback } from "multer";
+import { HTTP_STATUS_CODES } from "../types/http-status-codes";
 
 
 //Subir archivo
 const storage = diskStorage({
     destination: (req, file, cb) => {
         //Ruta  destino, para almacenar archivo
-        cb(null, 'public/images/');
+        cb(null, 'documents/');
     },
     filename:  (req, file, cb) => {
-        const ext = file.originalname.split('.').pop()?.toLocaleLowerCase();
+        const ext = file.originalname.split('.').pop()?.toLowerCase();
         const timestamp = new Date().getTime();
         cb(null, `${timestamp.toString()}.${(ext)}`);
     }
 });
 
-//Añadir filtros
+//Añadir filtro para validar que sea pdf
 const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback ) => {
-    cb(null, true);
+    if (file.mimetype === 'application/pdf') {
+        cb(null, true);
+    } else {
+        cb(new Error('Solo se permite archivos PDF'), false);
+    }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({ storage, fileFilter, limits: {fileSize: 5 * 1024 }}); //Limitar a 5 MB
 export default upload;
--- tarea3/src/routes/upload.ts
+++ tarea3/src/routes/upload.ts
@@ -1,24 +1,36 @@
 import { Router } from "express";
 import upload from '../middlewares/upload';
+import { HTTP_STATUS_CODES } from "../types/http-status-codes";
+
 
 const router = Router();
 
-router.post('/upload', upload.single('file') ,(req, res) => {
-    console.log('Archivo: ', req.file);
+//Subir un solo archivo
+router.post('/upload', upload.single('docs') ,(req, res) => {
+    //console.log('Archivo: ', req.file);
     if(req.file) {
-        res.send('Se subio el archivo');
+        res.status(HTTP_STATUS_CODES.SUCCESS).send({
+            filename: req.file.filename,
+            size: req.file.size,
+            path: req.file.path,
+        });
     } else {
-        res.status(400).send('No se subio el archivo');
+        res.status(HTTP_STATUS_CODES.BAD_REQUEST).send('No se subio el archivo');
     }
 });
 
 
-router.post('/uploads', upload.array('files'), (req, res) => {
-    console.log('Archivo: ', req.files);
-    if(req.files && req.files.length) {
-        res.send('se subieron los archivos');
+// Subida de varios archivos PDF
+router.post('/uploads', upload.array('docs', 5), (req, res) => {
+    if (req.files && req.files.length) {
+        const files = (req.files as Express.Multer.File[]).map(file => ({
+            filename: file.filename,
+            size: file.size,
+            path: file.path,
+        }));
+        res.status(HTTP_STATUS_CODES.SUCCESS).send({ files });
     } else {
-        res.status(400).send('No se subieron los archivos ');
+        res.status(HTTP_STATUS_CODES.BAD_REQUEST).send('No se subieron los archivos.');
     }
 });
 
