f11c4ca8d5908c95f1b87f840236aacd876ecb09
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-08 23:38:03 -0600

    Todo el proyecto de la tarea 3, problemas al hacer PR

---
+++ Tarea 3/src/types/http-status-codes.ts
@@ -0,0 +1,9 @@
+export enum HTTP_STATUS_CODES {
+    SUCCESS = 200,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    AUTHORIZATION = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    SERVER_ERROR = 500,
+}
